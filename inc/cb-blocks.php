802d9fcfc6a727b39038baf1f45855084eec7697
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-07-11 14:02:04 +0100

    all services block

--- inc/cb-blocks.php
+++ inc/cb-blocks.php
@@ -155,6 +155,15 @@ function acf_blocks()
             'mode'	=> 'edit',
             'supports' => array('mode' => false),
         ));
+        acf_register_block_type(array(
+            'name'				=> 'cb_all_services',
+            'title'				=> __('CB All Services'),
+            'category'			=> 'layout',
+            'icon'				=> 'cover-image',
+            'render_template'	=> 'page-templates/blocks/cb_all_services.php',
+            'mode'	=> 'edit',
+            'supports' => array('mode' => false),
+        ));
     }
 }
 add_action('acf/init', 'acf_blocks');
---
+++ page-templates/blocks/cb_all_services.php
@@ -0,0 +1,26 @@
+<section class="all_services py-5">
+    <div class="container-xl">
+        <div class="all_services__flex">
+            <a class="all_services__nav-item" href="/services/air-conditioning/">
+                <img src="<?=get_stylesheet_directory_uri()?>/img/icon__air-con.png" alt="">
+                <p>Air Conditioning</p>    
+            </a>
+            <a class="all_services__nav-item" href="/services/electrical/">
+                <img src="<?=get_stylesheet_directory_uri()?>/img/icon__electrical.png" alt="">
+                <p>Electrical</p>    
+            </a>
+            <a class="all_services__nav-item" href="/services/ventilation/">
+                <img src="<?=get_stylesheet_directory_uri()?>/img/icon__ventilation.png" alt="">
+                <p>Ventilation</p>    
+            </a>
+            <a class="all_services__nav-item" href="/services/refrigeration/">
+                <img src="<?=get_stylesheet_directory_uri()?>/img/icon__refrigeration.png" alt="">
+                <p>Refrigeration</p>    
+            </a>
+            <a class="all_services__nav-item" href="/services/heat-pumps/">
+                <img src="<?=get_stylesheet_directory_uri()?>/img/icon__heat-pump.png" alt="">
+                <p>Heat Pumps</p>    
+            </a>
+        </div>
+    </div>
+</section>
