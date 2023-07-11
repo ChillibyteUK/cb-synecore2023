649231eb6a540ba54081a6263b16b94ca26260aa
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-07-11 16:25:34 +0100

    some mobile fixes

--- page-templates/blocks/cb_latest_news.php
+++ page-templates/blocks/cb_latest_news.php
@@ -10,16 +10,19 @@ $q = wp_get_recent_posts(array(
         <div class="col-md-6 bg-blue text-md-end p-3 text-white"><h2 class="h3 mb-0">Latest News</h2></div>
     </div>
     <div class="container-xl">
-        <div class="row">
-            <div class="col-md-6">
-                <h3 class="h4"><?=$q[0]['post_title']?></h3>
+        <div class="row g-4">
+            <div class="col-md-6 d-flex flex-column justify-content-center">
+                <h3 class="h4 wrap"><?=$q[0]['post_title']?></h3>
                 <div class="text-end">
                     <a href="<?=get_the_permalink($q[0]['ID'])?>">Read More</a>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-5 offset-md-1">
                 <img src="<?=get_the_post_thumbnail_url($q[0]['ID'],'full')?>" alt="">
             </div>
+        </div>
+        <div class="text-center text-md-start">
+            <a href="/blog/" class="btn btn-secondary">View all news</a>
         </div>
     </div>
 </section>
