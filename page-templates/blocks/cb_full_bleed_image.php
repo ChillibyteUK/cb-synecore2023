05907633799bac8cfac256a1fa5d1d9f1a12a6ba
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-09-26 11:20:14 +0100

    Update cb_full_bleed_image.php

--- page-templates/blocks/cb_full_bleed_image.php
+++ page-templates/blocks/cb_full_bleed_image.php
@@ -1,5 +1,6 @@
 <?php
 $img = wp_get_attachment_image_url( get_field('image') ,'full');
+$class = $block['className'] ?? null ?: '';
 ?>
 <link rel="preload" as="image" href="<?=$img?>">
-<section class="full_bleed" style="background-image:url(<?=$img?>)"></section>
+<section class="full_bleed <?=$class?>" style="background-image:url(<?=$img?>)"></section>
