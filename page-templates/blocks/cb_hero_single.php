f0c65fafa8a2441961c3ebb73d4e74caf03addab
Matthew Pont <matthew.pont@gmail.com>
2024-02-05 10:27:00 +0000

    Added opacity to Hero

--- page-templates/blocks/cb_hero_single.php
+++ page-templates/blocks/cb_hero_single.php
@@ -6,7 +6,7 @@ $colour = get_field('theme') == 'Dark' ? 'bg-dark' : '';
 <link rel="preload" as="image" href="<?=$img?>">
 <header class="hero-single <?=$class?>" style="background-image:url(<?=$img?>)">
 <div class="hero-single__grid">
-    <div class="hero-single__content <?=$colour?>">
+    <div class="hero-single__content <?=$colour?> bg-opacity-75">
         <div class="hero-single__content--inner">
             <h1 class="mb-4"><?=get_field('title')?></h1>
             <div><?=get_field('content')?></div>
