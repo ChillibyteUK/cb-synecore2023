7a0f06288f50227f6d640fb443b5d8dd372579fa
Matthew Pont <matthew.pont@gmail.com>
2024-03-01 15:15:31 +0000

    Service Nav Styling

--- page-templates/blocks/cb_services_nav.php
+++ page-templates/blocks/cb_services_nav.php
@@ -16,10 +16,11 @@ $classes = (get_field('dark')[0] ?? null) == 'Yes' ? 'dark' : '';
         while (have_rows('services')) {
             the_row();
             $bg_colour = get_sub_field('bg_colour');
+            $paragraph_class = get_sub_field('paragraph_class');
             $s = get_sub_field('service');
             $img = get_sub_field('icon') ?: get_stylesheet_directory_uri() . '/img/missing-image.png';
             ?>
-            <a class="services__card text-white" href="<?=get_the_permalink($s)?>" style="background-color: <?=$bg_colour?>;">
+            <a class="services__card text-white <?=$paragraph_class?>" href="<?=get_the_permalink($s)?>" style="background-color: <?=$bg_colour?>;">
                 <h3><?=get_the_title($s)?></h3>
                 <p><?=get_sub_field('content')?></p>
                 <div class="icon_container"><img src="<?=$img?>"></div>
